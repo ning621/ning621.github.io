702d66fdc9ea024bdb7ea5b880e10bf626c4d1bd
郭晓宁 <guoxiaoning@cmcm.com>
2019-05-23 14:40:22 +0800

    init

---
+++ service-worker.js
@@ -0,0 +1,122 @@
+// https://github.com/14islands/vecka.14islands.com
+
+// Created using this tutorial
+// https://developers.google.com/web/fundamentals/primers/service-worker/?hl=en
+
+const CACHE_NAME = 'koot-sw-cache'
+const urlsToCache = [
+    "/includes/chunk.983a5934211e4993b655.js",
+    "/includes/chunk.b153b9ef4f33790464c2.js",
+    "/includes/chunk.f5268d576833194e6001.js",
+    "/includes/core.290209ec7e3413832f0d.js",
+    "/includes/core.cfa9fef8076c1f698a7b.js",
+    "/includes/core.e58b923c3b063b947f7a.js"
+]
+
+function addToCache(request, response) {
+    if (response.ok) {
+        const copy = response.clone()
+        caches.open(CACHE_NAME).then(cache => {
+            cache.put(request, copy)
+        })
+    } else {
+        console.log('Request fail', response, request)
+    }
+    return response
+}
+
+function fetchFromCache(event) {
+    return caches.match(event.request).then(response => {
+        if (!response) {
+            // A synchronous error that will kick off the catch handler
+            throw Error(`${event.request.url} not found in cache`)
+        }
+        return response
+    })
+}
+
+function offlineResponse() {
+    return new Response('Sorry, the application is offline.')
+}
+
+function respondFromNetworkThenCache(event) {
+    // console.log('Network first ', event.request.url)
+    // Check network first, then cache
+    const request = event.request
+    event.respondWith(
+        fetch(request)
+            .then(response => addToCache(request, response))
+            .catch(() => fetchFromCache(event))
+            .catch(() => offlineResponse())
+    )
+}
+
+function respondFromCacheThenNetwork(event) {
+    // console.log('Cache first ', event.request.url)
+    // Check cache first, then network
+    const request = event.request
+    event.respondWith(
+        fetchFromCache(event)
+            .catch(() => fetch(request))
+            .then(response => addToCache(request, response))
+            .catch(() => offlineResponse())
+    )
+}
+
+function shouldHandleFetch(event) {
+    if (event.request.method.toLowerCase() !== 'get') return false
+    if (event.request.url.indexOf(location.origin) < 0) return false
+    if (event.request.url.indexOf('google-analytics.com') !== -1) return false
+    if (event.request.url.indexOf(location.origin + '/api') > -1) return false
+    if (/\/service-worker(\.[a-z-_]+){0,1}\.js/i.test(event.request.url)) return false
+
+    return true
+}
+
+function shouldRespondFromNetworkThenCache(event) {
+    return (
+        event.request.headers.get('Accept').indexOf('text/html') >= 0
+        // || /chunk.+\.js$/.test(event.request.url)
+    )
+}
+
+// Open cache and store assets
+self.addEventListener('install', (event) => {
+    // console.log('Service Worker - INSTALL')
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                // console.log(urlsToCache)
+                return cache.addAll(urlsToCache);
+            })
+    )
+})
+
+self.addEventListener('fetch', (event) => {
+    if (shouldHandleFetch(event)) {
+        // console.log('Service Worker - FETCH', event.request)
+        if (shouldRespondFromNetworkThenCache(event)) {
+            respondFromNetworkThenCache(event)
+        } else {
+            respondFromCacheThenNetwork(event)
+        }
+    }
+})
+
+self.addEventListener('activate', (event) => {
+    // console.log('Service Worker - ACTIVATE')
+    var cacheWhitelist = [CACHE_NAME]
+    // Clean up old cache versions
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+    )
+})
